5c3c9428be4b7053b91d72e0a1b77f21c42b98c5
Igor Radkevich <landrommigor@gmail.com>
2023-07-24 12:08:49 +0300

    add new EF ModelDB

--- Model/DBModels/User.cs
+++ Model/DBModels/User.cs
@@ -5,8 +5,6 @@ namespace LibraryWPF.Model.DBModels;
 
 public partial class User
 {
-    public int Id { get; set; }
-
     public string Name { get; set; } = null!;
 
     public string LastName { get; set; } = null!;
