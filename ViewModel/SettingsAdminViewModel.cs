ae1f9b8cc20f5362cbea79a5ce45c71763302b8c
Igor Radkevich <landrommigor@gmail.com>
2023-08-01 10:22:26 +0300

    -Added buttons to expand the data editing panel

--- ViewModel/SettingsAdminViewModel.cs
+++ ViewModel/SettingsAdminViewModel.cs
@@ -3,22 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace LibraryWPF.ViewModel
 {
     public class SettingsAdminViewModel : ViewModelBase
     {
-        ICommand ShowTAutorsCommand { get; }
+        private bool _isClickedHeaderAutors = false;
+        private bool _isClickedHeaderRack = false;
+        private bool _isClickedHeaderReadPlace = false;  
+
+        public bool IsClickedHeaderAutors
+        {
+            get => _isClickedHeaderAutors;
+            set
+            {
+                _isClickedHeaderAutors = value;
+                OnPropertyChanged(nameof(IsClickedHeaderAutors));
+            }
+        }
+        public bool IsClickedHeaderRack
+        {
+            get => _isClickedHeaderRack;
+            set
+            {
+                _isClickedHeaderRack = value;
+                OnPropertyChanged(nameof(IsClickedHeaderRack));
+            }
+        }
+        public bool IsClickedHeaderReadPlace
+        {
+            get => _isClickedHeaderReadPlace;
+            set
+            {
+                _isClickedHeaderReadPlace= value;
+                OnPropertyChanged(nameof(IsClickedHeaderReadPlace));
+            }
+        }
+
+        public ICommand ClickHeaderAutorsCommand { get; }
+        public ICommand ClickHeaderRackCommand { get; }
+        public ICommand ClickHeaderReadPlaceCommand { get; }
 
         public SettingsAdminViewModel() 
         {
-            ShowTAutorsCommand = new ViewModelCommand(ExecuteShowTAutorsCommand);
+            ClickHeaderAutorsCommand = new ViewModelCommand(p => ExecuteClickHeaderAutorsCommand());
+            ClickHeaderRackCommand = new ViewModelCommand(p => ExecuteClickHeaderRackCommand());
+            ClickHeaderReadPlaceCommand = new ViewModelCommand(p => ExecuteClickHeaderReadPlaceCommand());
         }
 
-        private void ExecuteShowTAutorsCommand(object obj)
+        private void ExecuteClickHeaderReadPlaceCommand()
         {
+            IsClickedHeaderReadPlace = IsClickedHeaderReadPlace == true ? false : true;
+        }
 
+        private void ExecuteClickHeaderRackCommand()
+        {
+            IsClickedHeaderRack = IsClickedHeaderRack == true ? false : true;
+        }
+
+        private void ExecuteClickHeaderAutorsCommand()
+        {
+            IsClickedHeaderAutors = IsClickedHeaderAutors == true ? false : true;
         }
     }
 }
