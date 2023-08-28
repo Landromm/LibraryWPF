b5011ccbc2e3adfd1489eec43473ce01fdf51bee
Igor Radkevich <landrommigor@gmail.com>
2023-08-28 18:42:53 +0300

    -fix bug view MoreInfoListView Request

--- Model/RequestModel.cs
+++ Model/RequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,6 @@ namespace LibraryWPF.Model
         public string? UserName { get; set; }
         public string? UserLastName { get; set; }
 
-        public List<MoreRequestModel>? moreRequestModels { get; set; }
+        public ObservableCollection<MoreRequestModel>? moreRequestModels { get; set; }
     }
 }
